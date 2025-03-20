438f30c5ae0b03ef09b53aa6b76feaa347fafb3a
irtheman87 <ir2guru@gmail.com>
2025-03-19 17:45:28 -0700

    ttttttttt

--- src/utils/UtilityFunctions.ts
+++ src/utils/UtilityFunctions.ts
@@ -386,7 +386,7 @@ const storage = multerS3({
 });
 
 export const uploads = multer({ storage }).fields([
-  { name: 'files', maxCount: 1 },
+  { name: 'files', maxCount: 10 },
   { name: 'file', maxCount: 1 },
   { name: 'doc', maxCount: 1 },
   { name: 'additionalFile', maxCount: 5 },
